7ef8a86c5f86a88413e9b0f5a96e6e8609ae9f39
Fred <domaine75@gmail.com>
2017-09-08 10:09:30 +0100

    Add model and classes

--- RestDemo/RestDemo/Model/Customer.cs
+++ RestDemo/RestDemo/Model/Customer.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RestDemo.Model
 {
   public class Customer
-  {
-
+  {// [JsonProperty("odata.type")]
+    public string Type { set; get; }
+    public string CustomerID { get; set; }
+    public string CompanyName { get; set; }
+    public string ContactName { get; set; }
+    public string ContactTitle { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string Region { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public string Phone { get; set; }
+    public string Fax { get; set; }
   }
+
+  public class OData
+  {
+    [JsonProperty("odata.metadata")]
+    public string Metadata { get; set; }
+    public List<Customer> Value { get; set; }
+  }
+
+  //public class CustomerJson
+  //{
+  //    [JsonProperty("customer")]
+  //    public Customer Customer { get; set; }
+  //}
 }
--- RestDemo/RestDemo/Model/Order.cs
+++ RestDemo/RestDemo/Model/Order.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace RestDemo.Model
 {
   public class Order
   {
 
+    // [JsonProperty("odata.type")]
+    [JsonProperty("OrderID")]
+    //[JsonRequired]
+    public int OrderID { get; set; }
+
+    public string CustomerID { get; set; }
+    public Nullable<int> EmployeeID { get; set; }
+    public Nullable<System.DateTime> OrderDate { get; set; }
+    public Nullable<System.DateTime> RequiredDate { get; set; }
+    public Nullable<System.DateTime> ShippedDate { get; set; }
+    public Nullable<int> ShipVia { get; set; }
+    public Nullable<decimal> Freight { get; set; }
+    public string ShipName { get; set; }
+    public string ShipAddress { get; set; }
+    public string ShipCity { get; set; }
+    public string ShipRegion { get; set; }
+    public string ShipPostalCode { get; set; }
+    public string ShipCountry { get; set; }
+  }
+
+  public class ODataOrder
+  {
+    [JsonProperty("odata.metadata")]
+    public string Metadata { get; set; }
+    public List<Order> Value { get; set; }
   }
 }
